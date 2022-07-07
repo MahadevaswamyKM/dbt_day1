3f433b63d4285c3cabcb69e52d9de299298c5dfa
jceswamy@gmail.com <jceswamy@gmail.com>
2022-07-07 07:01:37 +0000

    2day commits

---
+++ tests/check_min.sql
@@ -0,0 +1,13 @@
+{{
+    config(
+        store_failures=True
+    )
+}}
+
+with orders as (
+    select * from {{ ref('stg_orders') }}
+)
+select order_id, sum(total_price) ordertotal 
+from orders 
+group by 1
+having (ordertotal<=1000)
